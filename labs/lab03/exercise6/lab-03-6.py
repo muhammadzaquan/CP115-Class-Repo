39dfb16a345f5aa3bfdc35f80638a0822efb7d99
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-06-30 15:40:55 +0800

    Finished exercise5 & exercise6

---
+++ labs/lab03/exercise6/lab-03-6.py
@@ -0,0 +1,8 @@
+yardLength = float(input())
+yardWidth = float(input())
+yardArea = yardLength * yardWidth
+houseLength = float(input())
+houseWidth = float(input())
+houseArea = houseLength * houseWidth
+wage = (yardArea - houseArea)/2
+print(wage)
