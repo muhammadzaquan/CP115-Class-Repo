9ae70e6ea967b14722b927d3bc2ad178470e4af9
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-07-06 09:23:03 +0800

    fixed error on lab04 exercise4

--- labs/lab04/exercise4/lab-4-4.py
+++ labs/lab04/exercise4/lab-4-4.py
@@ -3,7 +3,8 @@ baggage = int(input())
 if baggage > 0:
     weight = int(input())
     if weight >= 15:
-        chargeRate = ticketPrice + weight * chargeRate
+        chargeRate = 4
+        finalPrice = ticketPrice + weight * chargeRate
 else:
     discount = 10
     finalPrice = ticketPrice - discount
