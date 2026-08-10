7e010d196c2e6aa55f5f938ede835e887c4057e5
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-08-10 09:01:04 +0800

    modified:   labs/lab06/exercise1/exercise1.py and added a new file labs/lab06/lab_test_2.py with the following content:

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,21 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+print("========== RECEIPT ==========")
+print("\nItem\tPrice\tQty\tTotal\n")
+item1 = Coffee = 3.50
+qty = 2
+total = float(item1 * qty)
+print(f"Coffee\t$3.50\t{qty}\t${total:.2f}\n")
+item2 = Muffin = 2.10
+qty = 3
+total = float(item2 * qty)
+print(f"Muffin\t$2.10\t{qty}\t${total:.2f}\n")
+item3 = Water = 1.05
+qty = 4
+total = float(item3* qty)
+print(f"Water\t$1.05\t{qty}\t${total:.2f}\n")
+print("------------------------------")
+print(f"\nSubtotal:\t\t${(3.50*2)+(2.10*3)+(1.05*4):.2f}")
+tax = ((3.50*2)+(2.10*3)+(1.05*4)) * 0.06
+print(f"\nTax (6%)\t\t${tax:.2f}")
+print(f"\nTotal:\t\t\t${((3.50*2)+(2.10*3)+(1.05*4))+tax:.2f}")
+print("\n==============================")
+
---
+++ labs/lab06/lab_test_2.py
@@ -0,0 +1,3 @@
+assignment_marks = 10
+full_marks = assignment_marks * 2
+print(f"Name: Ahmad Bin Abu\t\t\tMatric. No: MS2025123499 \n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n\nThis is my\n\tsecond\n\t\tassignment\nI want 2x10 marks, which is {full_marks} full marks")
